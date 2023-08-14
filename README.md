2bbfceef02db031ab8267b0f4e000d7b7e54391e
pgrandne <perrin.grandne@protonmail.com>
2023-08-14 14:41:38 +0200

    add link for block explorer

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 # Deployments
 | Network               | Address                                          |
 |-----------------------|--------------------------------------------------|
-| Optimism Goerli |(0x47579267a80115b0fBc7b4FF35Cef538738dD036)[https://goerli-optimism.etherscan.io/address/0x47579267a80115b0fBc7b4FF35Cef538738dD036]      |
-| Base Goerli|0x6925A6A9b4e73769734F4644DE07169FBC5f0C57   |
+| Optimism Goerli |[0x47579267a80115b0fBc7b4FF35Cef538738dD036](https://goerli-optimism.etherscan.io/address/0x47579267a80115b0fBc7b4FF35Cef538738dD036) |
+| Base Goerli |[0x6925A6A9b4e73769734F4644DE07169FBC5f0C57]()  |
 | Mode Goerli |0xb577ED8dB5a965815b53d5141A83D1DEed438DBC|
-| Zora   |    |
 
--- core/hardhat.config.js
+++ core/hardhat.config.js
@@ -8,7 +8,7 @@ const { API_URL, PRIVATE_KEY } = process.env;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.18",
   defaultNetwork: "goerli",
   networks: {
       hardhat: {},
@@ -19,11 +19,27 @@ module.exports = {
       optimismGoerli: {
         url: API_URL,
         accounts: [`0x${PRIVATE_KEY}`]
-      }
+      },
+          // for testnet
+      'base-goerli': {
+        url: 'https://goerli.base.org',
+        accounts: [`0x${PRIVATE_KEY}`],
+        gasPrice: 1000000000,
+      },
   },
   etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: {
+      "base-goerli": "PLACEHOLDER_STRING"
+     },
+     customChains: [
+       {
+         network: "base-goerli",
+         chainId: 84531,
+         urls: {
+          apiURL: "https://api-goerli.basescan.org/api",
+          browserURL: "https://goerli.basescan.org"
+         }
+       }
+     ]
   }
 };
